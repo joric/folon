5c22dbe1ac8039c144b3b9558120abcbe2bbc608
joric <joric@users.noreply.github.com>
2025-11-13 18:45:53 +0500

    updated files

--- scripts/export_sprites.cmd
+++ scripts/export_sprites.cmd
@@ -3,15 +3,21 @@
 rem 1) use [FO4Tools]() to extract "LondonWorldSpace - Interface.ba2\Interface\Pipboy_MapPage.swf"
 rem 2) use [JPEX](https://github.com/jindrapetrik/jpexs-decompiler/releases/) to save shapes from Pipboy_MapPage.swf
 
-set fo4=E:\Games\Fallout 4 GOTY
 set outdir=.\export
 
+set fo4=E:\Games\Fallout 4 GOTY
+
+rem extract swf
+
 set path=D:\Shared\Tools\Hacking\Games\Bethesda\F4SE\fallout4_tools;%path%
-set path=D:\Shared\Tools\Hacking\Games\Bethesda\JPEXS;%path%
 
 set ba2=%fo4%\data\LondonWorldSpace - Interface.ba2
 
 ba2extract.exe "%ba2%" "%outdir%" && del ba2extract.log
+
+rem export sprites
+
+set path=D:\Shared\Tools\Hacking\Games\Bethesda\JPEXS;%path%
 
 set swf=%outdir%\Interface\Pipboy_MapPage.swf
 
