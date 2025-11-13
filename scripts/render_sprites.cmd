0dd79aecf33d39c93a2c2b98c42f30001686eec2
joric <joric@users.noreply.github.com>
2025-11-13 13:44:33 +0500

    updated files

--- scripts/render_sprites.cmd
+++ scripts/render_sprites.cmd
@@ -1,10 +1,11 @@
 @echo off
 
-mkdir sprites
+mkdir sprites 2>nul
 
 setlocal enabledelayedexpansion
 for %%F in (.\export\shapes\*.svg) do (
-    magick -density 180 -background none "%%F" -gravity center -extent 64x64 "sprites\%%~nF.png"
+    rem magick -density 192 -background none "%%F" -gravity center -extent 64x64 "sprites\%%~nF.png"
+    magick -density 192 -background none "%%F" -gravity center -extent 64x64 -alpha set -fill "rgba(255,255,255,0.1)" -opaque none -fill "rgba(0,0,0,0)" -draw "color 0,0 floodfill" "..\sprites\%%~nF.png"
 )
 
 
